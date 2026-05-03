790218b0b335638e6565a13ff789ee2b60e5f32c
mar1anDEV <marianhalibei.dev@gmail.com>
2026-05-03 15:43:21 +0200

    commit

--- src/components/layout/SidebarLayout.tsx
+++ src/components/layout/SidebarLayout.tsx
@@ -1,6 +1,6 @@
 
-import latestNewsBannerUI from "../ui/latestNewsBannerUI";
-import classifiedsBannerUI from "../ui/classifiedsBannerUI";
+import LatestNewsBannerUI from "../ui/LatestNewsBannerUI";
+import ClassifiedsBannerUI from "../ui/ClassifiedsBannerUI";
 
 function SidebarLayout() {
 
@@ -10,8 +10,8 @@ function SidebarLayout() {
   return (
     <div className="xl:container mx-auto px-4 py-8 bg-gray-200 rounded my-2">
         <div className="flex flex-col md:flex-row gap-6 space-y-6 h-auto md:space-y-0">
-            {latestNewsBannerUI()}
-            {classifiedsBannerUI()}
+            <LatestNewsBannerUI/>
+            <ClassifiedsBannerUI/>
         </div>
     </div>
   )
