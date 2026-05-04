bbb072a02fa9c4c62841d6e5d9d6ad8ac069c244
mar1anDEV <marianhalibei.dev@gmail.com>
2026-05-04 20:56:57 +0200

    fix

--- src/components/ui/buttons/BtnClassifieds.tsx
+++ src/components/ui/buttons/BtnClassifieds.tsx
@@ -11,9 +11,10 @@ interface Props{
 function BtnClassifieds({actionLabel,path,addBtn,classCustom}:Props) {
   return (
     <button type='button' className={`${classCustom} `}>
-        <Link to={path} className={`text-md mt-6 sm:mt-0 px-4 py-2 rounded bg-blue-900 hover:bg-blue-600 transition-colors duration-300 text-white font-bold flex flex-row items-center justify-center gap-x-2.5`}>
+        <Link to={""} className={`text-md mt-6 sm:mt-0 px-4 py-2 rounded bg-blue-900 hover:bg-blue-600 transition-colors duration-300 text-white font-bold flex flex-row items-center justify-center gap-x-2.5`}>
             {addBtn && (<span>+</span>)}
             <span>{actionLabel}</span>
+            <span className='hidden'>{path}</span>
         </Link>
     </button>
   )
--- src/components/ui/filters/CategoryListingUI.tsx
+++ src/components/ui/filters/CategoryListingUI.tsx
@@ -15,13 +15,13 @@ interface Props{
     }[]
 }
 
-function CategoryListingUI({title,listing,onItemClick,activeItem}: Props) {
+function CategoryListingUI({title,listing,onItemClick}: Props) {
   return (
     <div className='flex flex-col gap-2 mt-2'>
         <div className='aria-title'><h4 className='text-black font-bold text-md'>{title}</h4></div>
         <div className='flex-flex-col'>
           {listing?.map((item)=>(
-            <ItemListingClassifieds isActive={activeItem} onClick={()=> item?.id && onItemClick?.(item.id)}  idItem={item.id} icon={item.icon} itemName={item.name} key={item.id} />
+            <ItemListingClassifieds  onClick={()=> item?.id && onItemClick?.(item.id)}  idItem={item.id} icon={item.icon} itemName={item.name} key={item.id} />
           ))}
         </div>
     </div>
